d4582e211fce52fb0034bf9ab458a9356a77f34c
sandbarTrue <1253641040@qq.com>
2020-04-19 17:34:10 +0800

    bio

---
+++ src/test/java/com/example/dayliy/learn/DayliyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dayliy.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayliyLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
